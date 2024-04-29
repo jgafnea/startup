c7d41af76097200a549a5199d65505847747b4f4
jgafnea <jgafnea@mail.com>
2024-04-29 15:47:15 -0500

    feat: faker bs

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
+from faker import Faker
 from flask import Flask
 
 app = Flask(__name__)
+fake = Faker()
+
+
+def get_bs() -> tuple:
+    first, second = [fake.bs() for _ in range(2)]
+    return first, second
+
+
+@app.route("/")
+def get_index() -> str:
+    first, second = get_bs()
+    statement = f"{first.capitalize()} and {second}"
+    return statement
+
 
 if __name__ == "__main__":
     app.run()
