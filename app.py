f7778decf926ba0f56051b9bb94b7f96bc552868
John Gafnea <jgafnea@mail.com>
2024-08-29 12:53:12 -0500

    chore: cleanup

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 import requests
-from faker import Faker
 from flask import Flask, render_template
 
 app = Flask(__name__)
-fake = Faker()
 
 
 @app.route("/")
 def get_index() -> str:
-    # Use Faker for header
-    first, second = [fake.bs() for _ in range(2)]
+    # Use Faker/Mocker API for header. Call twice and combine responses with "and".
+    response = requests.get("https://api.mockster.dev/api/v1/companies").json()
+    first, second = [response[i].get("buzzPhrase") for i in range(2)]
     header = f"{first.title()} and {second.title()}"
 
-    # Use API for paragraph
+    # Use ITFT API for paragraph.
     response = requests.get("https://itsthisforthat.com/api.php?json").json()
     this, that = response.values()
     paragraph = f"Basically it's {this} for {that}"
